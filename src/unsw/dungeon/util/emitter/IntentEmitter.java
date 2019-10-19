5cc105ae89f346d4d2f980fa493327df37c280a1
Andrew Wong <andrew.j.wong@outlook.com>
2019-10-19 21:41:40 +1100

    Fix event listener firing
    Create a copy of the events before firing, so that the list of future events can be modified

--- src/unsw/dungeon/util/emitter/IntentEmitter.java
+++ src/unsw/dungeon/util/emitter/IntentEmitter.java
@@ -1,5 +1,7 @@
 package unsw.dungeon.util.emitter;
 
+import java.util.ArrayList;
+
 public class IntentEmitter<BaseType, DataType extends EmitterData>
 		extends Emitter<Intent<BaseType, DataType>, BaseType, DataType> {
 
@@ -9,7 +11,12 @@ public class IntentEmitter<BaseType, DataType extends EmitterData>
 
 	@Override
 	public boolean emit(DataType data) {
+		ArrayList<Intent<BaseType, DataType>> localSubcribers = new ArrayList<Intent<BaseType, DataType>>();
 		for (Intent<BaseType, DataType> subscriber : this.subscribers) {
+			localSubcribers.add(subscriber);
+		}
+
+		for (Intent<BaseType, DataType> subscriber : localSubcribers) {
 			if (!subscriber.execute(this.reference, data)) {
 				return false;
 			}
