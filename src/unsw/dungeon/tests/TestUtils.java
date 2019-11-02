a3760bd44a67abfa1a715ec614a214c32ca80ea1
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-02 23:54:24 +1100

    Tests for US5

---
+++ src/unsw/dungeon/tests/TestUtils.java
@@ -0,0 +1,94 @@
+package unsw.dungeon.tests;
+
+import unsw.dungeon.Dungeon;
+import unsw.dungeon.GameHooks;
+import unsw.dungeon.entity.Boulder;
+import unsw.dungeon.entity.Door;
+import unsw.dungeon.entity.Enemy;
+import unsw.dungeon.entity.Exit;
+import unsw.dungeon.entity.Portal;
+import unsw.dungeon.entity.Switch;
+import unsw.dungeon.entity.Sword;
+import unsw.dungeon.entity.Wall;
+
+public class TestUtils {
+	private Dungeon dungeon;
+	private GameHooks gameHooks;
+
+	public TestUtils(Dungeon dungeon) {
+		this.dungeon = dungeon;
+		this.gameHooks = new GameHooks();
+	}
+
+	Boulder Boulder(int x, int y) {
+		Boulder boulder = new Boulder(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(boulder);
+		dungeon.addEntity(boulder);
+
+		return boulder;
+	}
+
+	Switch Switch(int x, int y) {
+		Switch switchEntity = new Switch(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(switchEntity);
+		dungeon.addEntity(switchEntity);
+
+		return switchEntity;
+	}
+
+	Portal Portal(int x, int y) {
+		Portal portal = new Portal(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(portal);
+		dungeon.addEntity(portal);
+
+		return portal;
+	}
+
+	Enemy Enemy(int x, int y) {
+		Enemy enemy = new Enemy(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(enemy);
+		dungeon.addEntity(enemy);
+
+		return enemy;
+	}
+
+	Sword Sword(int x, int y) {
+		Sword sword = new Sword(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(sword);
+		dungeon.addEntity(sword);
+
+		return sword;
+	}
+
+	Wall Wall(int x, int y) {
+		Wall wall = new Wall(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(wall);
+		dungeon.addEntity(wall);
+
+		return wall;
+	}
+
+	Exit Exit(int x, int y) {
+		Exit exit = new Exit(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(exit);
+		dungeon.addEntity(exit);
+
+		return exit;
+	}
+
+	Door Door(int x, int y) {
+		Door door = new Door(this.dungeon, x, y);
+
+		this.gameHooks.onLoad(door);
+		dungeon.addEntity(door);
+
+		return door;
+	}
+}
---
+++ src/unsw/dungeon/tests/US5Test.java
@@ -0,0 +1,209 @@
+package unsw.dungeon.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import unsw.dungeon.Dungeon;
+import unsw.dungeon.entity.Boulder;
+import unsw.dungeon.entity.Door;
+import unsw.dungeon.entity.Enemy;
+import unsw.dungeon.entity.Exit;
+import unsw.dungeon.entity.Player;
+import unsw.dungeon.entity.Portal;
+import unsw.dungeon.entity.Switch;
+import unsw.dungeon.entity.Sword;
+import unsw.dungeon.entity.Wall;
+import unsw.dungeon.entity.meta.Entity;
+
+class US5Test {
+
+	private Dungeon dungeon;
+	private Player player;
+	private TestUtils Create;
+
+	@BeforeEach
+	void init() {
+		dungeon = new Dungeon(10, 10);
+		dungeon.addEntity((player = new Player(dungeon, 1, 1)));
+		dungeon.setPlayer(player);
+		Create = new TestUtils(dungeon);
+	}
+
+	@Test
+	void boulderPush() {
+		Boulder boulder = Create.Boulder(2, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+
+		// TODO Switch event
+	}
+
+	@Test
+	void boulderPushSwitch() {
+		Boulder boulder = Create.Boulder(2, 1);
+		Switch switchEntity = Create.Switch(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+
+		assertEquals(switchEntity.getX(), 3);
+		assertEquals(switchEntity.getY(), 1);
+	}
+
+	@Test
+	void boulderPushPortal() {
+		Boulder boulder = Create.Boulder(2, 1);
+		Portal portal = Create.Portal(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+
+		assertEquals(portal.getX(), 3);
+		assertEquals(portal.getY(), 1);
+	}
+
+	@Test
+	void boulderPushExit() {
+		Boulder boulder = Create.Boulder(2, 1);
+		Exit exit = Create.Exit(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+	}
+
+	@Test
+	void boulderPushEnemy() {
+		Boulder boulder = Create.Boulder(2, 1);
+		Enemy enemy = Create.Enemy(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+
+		// Enemy should die
+		for (Entity e : dungeon.getEntities()) {
+			if (e == enemy) {
+				fail();
+			}
+		}
+	}
+
+	@Test
+	void boulderPushItems() {
+		// Test a sword
+		Boulder boulder = Create.Boulder(2, 1);
+		Sword sword = Create.Sword(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+
+		for (Entity e : dungeon.getEntities()) {
+			if (e == sword) {
+				return;
+			}
+		}
+
+		fail();
+	}
+
+	@Test
+	void boulderPushWall() {
+		Boulder boulder = Create.Boulder(2, 1);
+		Wall wall = Create.Wall(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		player.moveRight();
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+	}
+
+	@Test
+	void boulderPushDoor() {
+		Boulder boulder = Create.Boulder(2, 1);
+		Door door = Create.Door(3, 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		// Test with a close door, should block
+		player.moveRight();
+		assertEquals(player.getX(), 1);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder.getX(), 2);
+		assertEquals(boulder.getY(), 1);
+
+		// Test with an open door, should allow
+		door.open();
+
+		player.moveRight();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder.getX(), 3);
+		assertEquals(boulder.getY(), 1);
+	}
+
+	@Test
+	void boulderPushBoulder() {
+		Boulder boulder1 = Create.Boulder(2, 1);
+
+		assertEquals(boulder1.getX(), 2);
+		assertEquals(boulder1.getY(), 1);
+
+		player.moveRight();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder1.getX(), 3);
+		assertEquals(boulder1.getY(), 1);
+
+		Boulder boulder2 = Create.Boulder(4, 1);
+		player.moveRight();
+		assertEquals(player.getX(), 2);
+		assertEquals(player.getY(), 1);
+
+		assertEquals(boulder2.getX(), 4);
+		assertEquals(boulder2.getY(), 1);
+
+	}
+
+}
