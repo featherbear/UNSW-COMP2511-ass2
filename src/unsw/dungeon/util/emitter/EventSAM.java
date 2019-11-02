e50fbf24ba4280e58d9f6f70cb8a28fe24668b8a
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-02 19:20:03 +1100

    Refactor unsw.dungeon.util.emitter.*

--- src/unsw/dungeon/util/emitter/EventSAM.java
+++ src/unsw/dungeon/util/emitter/EventSAM.java
@@ -1,5 +1,11 @@
 package unsw.dungeon.util.emitter;
 
+/**
+ * SAM Interface for data events
+ *
+ * @param <BaseType>
+ * @param <DataType>
+ */
 @FunctionalInterface
 public interface EventSAM<BaseType, DataType extends EmitterData> {
 	public void execute(BaseType obj, DataType data);
