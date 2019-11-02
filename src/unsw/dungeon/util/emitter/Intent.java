26ff0aa15566b97bf5a94ac8b23eac4f2969a914
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-02 13:12:57 +1100

    Add @FunctionalInterface decorator

--- src/unsw/dungeon/util/emitter/Intent.java
+++ src/unsw/dungeon/util/emitter/Intent.java
@@ -1,5 +1,6 @@
 package unsw.dungeon.util.emitter;
 
+@FunctionalInterface
 public interface Intent<BaseType, DataType extends EmitterData> {
 	public boolean execute(BaseType obj, DataType data);
 }
